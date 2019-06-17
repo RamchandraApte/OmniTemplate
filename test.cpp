eac75cbc6fda42a3ebd1d959e4a68192a60e55a3
Ramchandra Apte <maniandram01@gmail.com>
2019-06-17 16:42:42 +0530

    Added matrix division test.

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include "template.h"
+void test_matrix(){
+	mat<ld> a{{2,3,5}, {3,6,10}, {5,9,16}};
+	mat<ld> b{{1,2,3}, {4,5,6}, {9,7,8}};
+	auto x = b/a;
+	dbg(x);
+	dbg(a*x);
+	dbg(b);
+}
+void main2(){
+	test_matrix();
+}
