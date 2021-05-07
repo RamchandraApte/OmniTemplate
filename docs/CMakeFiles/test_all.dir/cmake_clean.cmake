f150f451871b0ab43ac39a798186278106da1527
Ramchandra Apte <maniandram01@gmail.com>
2021-05-07 18:05:32 -0400

    Add all docs file

---
+++ docs/CMakeFiles/test_all.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_all"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/test_all.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
